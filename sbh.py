d23352bd346d85377a58d4e3f13e89ba15b80ada
OGLinuk <daniel-l-black@protonmail.com>
2019-04-02 00:17:40 +1300

    Adding automated rotations and seed for replicability

--- sbh.py
+++ sbh.py
@@ -1,32 +1,38 @@
 import os
+import random
 import sys
 from string import ascii_lowercase, digits, printable
 import hashlib
 
 SYMBOLS = printable[62:]
 
-def rotFunc(prRotation, prPlainText):
-    prPlainText = prPlainText.replace(' ', '')
-    encryptedText = ''
-    for n in prPlainText:
+def rotFunc(rot, plaintext):
+    plaintext = plaintext.replace(' ', '')
+    encrypted = ''
+    for n in plaintext:
         if n.isalpha():
-            encryptedText += ascii_lowercase[(ascii_lowercase.index(n.lower()) + prRotation) % len(ascii_lowercase)]
+            encrypted += ascii_lowercase[(ascii_lowercase.index(n.lower()) + rot) % len(ascii_lowercase)]
         elif n.isdigit():
-            encryptedText += digits[(digits.index(n) + prRotation) % len(digits)]
+            encrypted += digits[(digits.index(n) + rot) % len(digits)]
         else:
-            encryptedText += SYMBOLS[(SYMBOLS.index(n) + prRotation) % len(SYMBOLS)]
-    return hashlib.sha256(bytes(encryptedText, 'UTF-8')).hexdigest()
+            encrypted += SYMBOLS[(SYMBOLS.index(n) + rot) % len(SYMBOLS)]
+    return hashlib.sha256(bytes(encrypted, 'UTF-8')).hexdigest()
 
 def main():
     while True:
         os.system('cls' if os.name == 'nt' else 'clear')
         print('-----------------Salt Bae Hash-----------------')
-        print('Plain Text -> Cipher(s) -> Hash(s)')
+        print('Plain Text -> N Cipher(s) -> Seed -> Hash(s)')
         plainText = input('Plain Text: ')
-        iters = int(input('Num of ciphers: '))
-        text = rotFunc(int(input('Rotation: ')), plainText)
-        for i in range(1, iters):
-            text = rotFunc(int(input('Rotation: ')), text)
+        iters = int(input('Number of Ciphers: '))
+        random.seed(int(input('Seed: ')))
+        rot = random.randint(1, sys.maxsize)
+        print(rot)
+        text = rotFunc(rot, plainText)
+        for _ in range(1, iters):
+            random.seed(rot)
+            rot = random.randint(1, sys.maxsize)
+            text = rotFunc(rot, text)
         print('Salt Bae Encrypted String: {}'.format(text))
         input('\nPress enter to sbh another plain text input ...')
 
